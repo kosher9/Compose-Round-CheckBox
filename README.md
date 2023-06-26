54889d867395f58e5e5c16047196880077139ada
kosher9 <lionel.kossi@gmail.com>
2023-06-26 15:42:51 +0000

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Compose_Nice_CheckBox
+# Compose-Round-CheckBox
+[![](https://jitpack.io/v/kosher9/Compose-Round-CheckBox.svg)](https://jitpack.io/#kosher9/Compose-Round-CheckBox)
+
+RoundCheckboxes allow users to select one or more items from a set. RoundCheckboxes can turn an option on or off.
+
+## Preview
+https://github.com/kosher9/Compose-Round-CheckBox/assets/34873327/dee17090-e20a-4309-9595-2b16f9028206
+
+## Gradle Setup
+
+To get a Git project into your build:
+
+* Step 1. Add the JitPack repository to your build file Add it in your root build.gradle at the end
+  of repositories:
+
+```
+allprojects {
+  repositories {
+      ...
+      maven { url 'https://jitpack.io' }
+  }
+}
+```
+
+* Step 2. Add the dependency
+
+```
+dependencies {
+	  implementation 'com.github.kosher9:Compose-Round-CheckBox:Tag'
+}
+```
+
+## 📝 License
+
+This project is [MIT](./LICENSE.md) licensed.
--- app/src/main/java/com/kosher9/app/MainActivity.kt
+++ app/src/main/java/com/kosher9/app/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,10 +22,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kosher9.app.ui.theme.ComposeNiceCheckBoxTheme
 import com.kosher9.roundcheckbox.RoundCheckBox
+import com.kosher9.roundcheckbox.RoundCheckBoxDefaults
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.Q)
@@ -38,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RoundCheckBoxes()
                 }
             }
         }
@@ -46,41 +48,103 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun RoundCheckBoxes(modifier: Modifier = Modifier) {
 
-    var radioButtonState by remember { mutableStateOf(false) }
-    var niceCheckBoxState by remember { mutableStateOf(false) }
+    var roundCheckBoxEnableState by remember { mutableStateOf(false) }
+    var roundCheckBoxDisableState by remember { mutableStateOf(false) }
+    var roundCheckBoxCheckedDisableState by remember { mutableStateOf(true) }
+    var customRoundCheckBoxDisableState by remember { mutableStateOf(false) }
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(5.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
+    Column(modifier = Modifier.padding(top = 50.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Enabled",
+                modifier = modifier
+            )
 
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-//        Checkbox(checked = , onCheckedChange = )
+            RoundCheckBox(
+                modifier = Modifier.width(60.dp),
+                isChecked = roundCheckBoxEnableState,
+                onClick = { roundCheckBoxEnableState = !roundCheckBoxEnableState },
+                enabled = true
+            )
+        }
 
-        RadioButton(selected = radioButtonState, onClick = { radioButtonState = !radioButtonState })
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Disabled",
+                modifier = modifier
+            )
 
-        RoundCheckBox(
-            modifier = Modifier.width(60.dp),
-            radius = 10.dp,
-            isChecked = niceCheckBoxState,
-            onClick = { niceCheckBoxState = !niceCheckBoxState },
-            enabled = true
-        )
+            RoundCheckBox(
+                modifier = Modifier.width(60.dp),
+                isChecked = roundCheckBoxDisableState,
+                onClick = { roundCheckBoxDisableState = !roundCheckBoxDisableState },
+                enabled = false
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Disabled & Checked",
+                modifier = modifier
+            )
+
+            RoundCheckBox(
+                modifier = Modifier.width(60.dp),
+                isChecked = roundCheckBoxCheckedDisableState,
+                onClick = { roundCheckBoxCheckedDisableState = !roundCheckBoxCheckedDisableState },
+                enabled = false
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Custom",
+                modifier = modifier
+            )
+
+            RoundCheckBox(
+                modifier = Modifier.width(60.dp),
+                radius = 10.dp,
+                color = RoundCheckBoxDefaults.colors(Color(20, 138, 254), Color(0, 0, 0), Color(20, 138, 254), Color.White, Color(20, 138, 254)),
+                isChecked = customRoundCheckBoxDisableState,
+                onClick = { customRoundCheckBoxDisableState = !customRoundCheckBoxDisableState },
+                enabled = true
+            )
+        }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeNiceCheckBoxTheme {
-        Greeting("Android")
+        RoundCheckBoxes()
     }
 }
